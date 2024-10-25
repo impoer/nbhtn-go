26d6a76a8a84187a5be2a4b858c403c0712f1185
Roma <impoe@ukr.net>
2024-10-25 23:18:33 +0300

    try to fix docker doploy on ec2

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"user-auth/internal/auth"
 	"user-auth/internal/db"
@@ -11,33 +13,31 @@ import (
 	"github.com/gorilla/mux"
 )
 
-const (
-	host     = "db"
-	port     = 5432 // Держим port как int
-	user     = "postgres"
-	password = "swagimpoe123"
-	dbname   = "auth_db"
-)
-
 func main() {
-	// Инициализация базы данных
-	database := db.InitDB(host, port, user, password, dbname) // Передаем port как int
-	defer database.Close()                                    // Закрыть соединение после использования
+    host := os.Getenv("DATABASE_HOST")
+    port, _ := strconv.Atoi(os.Getenv("DATABASE_PORT"))
+    user := os.Getenv("DATABASE_USER")
+    password := os.Getenv("DATABASE_PASSWORD")
+    dbname := os.Getenv("DATABASE_NAME")
 
-	auth.SetDB(database) // Установить базу данных в пакет auth
+    // Инициализация базы данных
+    database := db.InitDB(host, port, user, password, dbname) // Передаем port как int
+    defer database.Close()                                    // Закрыть соединение после использования
 
-	r := mux.NewRouter()
+    auth.SetDB(database) // Установить базу данных в пакет auth
 
-	r.HandleFunc("/register", auth.RegisterUser).Methods("POST")
-	r.HandleFunc("/login", auth.LoginUser).Methods("POST")
-	r.HandleFunc("/users", auth.TokenValid(auth.GetAllUsers)).Methods("GET")
-	r.HandleFunc("/user/{id}", auth.TokenValid(auth.GetUserByID)).Methods("GET")
-	r.HandleFunc("/refresh", auth.RefreshAccessToken).Methods("POST")
+    r := mux.NewRouter()
 
-	allowedOrigins := handlers.AllowedOrigins([]string{"http://localhost:3000"})
-	allowedMethods := handlers.AllowedMethods([]string{"POST", "GET", "OPTIONS"})
-	allowedHeaders := handlers.AllowedHeaders([]string{"Content-Type", "Authorization"})
+    r.HandleFunc("/register", auth.RegisterUser).Methods("POST")
+    r.HandleFunc("/login", auth.LoginUser).Methods("POST")
+    r.HandleFunc("/users", auth.TokenValid(auth.GetAllUsers)).Methods("GET")
+    r.HandleFunc("/user/{id}", auth.TokenValid(auth.GetUserByID)).Methods("GET")
+    r.HandleFunc("/refresh", auth.RefreshAccessToken).Methods("POST")
 
-	log.Println("Server running on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(allowedOrigins, allowedMethods, allowedHeaders)(r)))
+    allowedOrigins := handlers.AllowedOrigins([]string{"http://localhost:3000"})
+    allowedMethods := handlers.AllowedMethods([]string{"POST", "GET", "OPTIONS"})
+    allowedHeaders := handlers.AllowedHeaders([]string{"Content-Type", "Authorization"})
+
+    log.Println("Server running on port 8080")
+    log.Fatal(http.ListenAndServe(":8080", handlers.CORS(allowedOrigins, allowedMethods, allowedHeaders)(r)))
 }
