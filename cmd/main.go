d8414b973ef576f065c70132c22533dbb87c5cb6
Roma <impoe@ukr.net>
2024-10-26 00:03:03 +0300

    stuff

--- cmd/main.go
+++ cmd/main.go
@@ -14,30 +14,38 @@ import (
 )
 
 func main() {
-    host := os.Getenv("DATABASE_HOST")
-    port, _ := strconv.Atoi(os.Getenv("DATABASE_PORT"))
-    user := os.Getenv("DATABASE_USER")
-    password := os.Getenv("DATABASE_PASSWORD")
-    dbname := os.Getenv("DATABASE_NAME")
+	host := os.Getenv("DATABASE_HOST")
+	port, _ := strconv.Atoi(os.Getenv("DATABASE_PORT"))
+	user := os.Getenv("DATABASE_USER")
+	password := os.Getenv("DATABASE_PASSWORD")
+	dbname := os.Getenv("DATABASE_NAME")
 
-    // Инициализация базы данных
-    database := db.InitDB(host, port, user, password, dbname) // Передаем port как int
-    defer database.Close()                                    // Закрыть соединение после использования
+	// Логирование параметров подключения к базе данных
+	log.Println("Database Connection Parameters:")
+	log.Printf("Host: %s", host)
+	log.Printf("Port: %d", port)
+	log.Printf("User: %s", user)
+	log.Printf("Password: %s", password) // Будьте осторожны с логированием паролей!
+	log.Printf("Database Name: %s", dbname)
 
-    auth.SetDB(database) // Установить базу данных в пакет auth
+	// Инициализация базы данных
+	database := db.InitDB(host, port, user, password, dbname) // Передаем port как int
+	defer database.Close()                                    // Закрыть соединение после использования
 
-    r := mux.NewRouter()
+	auth.SetDB(database) // Установить базу данных в пакет auth
 
-    r.HandleFunc("/register", auth.RegisterUser).Methods("POST")
-    r.HandleFunc("/login", auth.LoginUser).Methods("POST")
-    r.HandleFunc("/users", auth.TokenValid(auth.GetAllUsers)).Methods("GET")
-    r.HandleFunc("/user/{id}", auth.TokenValid(auth.GetUserByID)).Methods("GET")
-    r.HandleFunc("/refresh", auth.RefreshAccessToken).Methods("POST")
+	r := mux.NewRouter()
 
-    allowedOrigins := handlers.AllowedOrigins([]string{"http://localhost:3000"})
-    allowedMethods := handlers.AllowedMethods([]string{"POST", "GET", "OPTIONS"})
-    allowedHeaders := handlers.AllowedHeaders([]string{"Content-Type", "Authorization"})
+	r.HandleFunc("/register", auth.RegisterUser).Methods("POST")
+	r.HandleFunc("/login", auth.LoginUser).Methods("POST")
+	r.HandleFunc("/users", auth.TokenValid(auth.GetAllUsers)).Methods("GET")
+	r.HandleFunc("/user/{id}", auth.TokenValid(auth.GetUserByID)).Methods("GET")
+	r.HandleFunc("/refresh", auth.RefreshAccessToken).Methods("POST")
 
-    log.Println("Server running on port 8080")
-    log.Fatal(http.ListenAndServe(":8080", handlers.CORS(allowedOrigins, allowedMethods, allowedHeaders)(r)))
+	allowedOrigins := handlers.AllowedOrigins([]string{"http://localhost:3000"})
+	allowedMethods := handlers.AllowedMethods([]string{"POST", "GET", "OPTIONS"})
+	allowedHeaders := handlers.AllowedHeaders([]string{"Content-Type", "Authorization"})
+
+	log.Println("Server running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(allowedOrigins, allowedMethods, allowedHeaders)(r)))
 }
